71a763ae41e81cefd2e194c0440556e71d4cb796
Travis Stratton <t.strat7@gmail.com>
2023-08-21 10:47:10 -0500

    Change url to localhost from https to http

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/your-database-name';
+const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/your-database-name';
 
 const db = new Client({
     connectionString,
