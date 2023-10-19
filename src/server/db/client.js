92dc3374807389b07f3e8b97f8609f7e12c2fbdc
Jack-Tillman <johntillman345@gmail.com>
2023-10-19 13:19:19 -0400

    Set up database named capstone-db, installed packages, each team member must make their own .env and add the JWT token, everything should be good to start

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/your-database-name';
+const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/capstone-db';
 
 const db = new Client({
     connectionString,
