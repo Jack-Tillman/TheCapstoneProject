4a7c9f38e7686e8414f0dad7d1f55e5be2e37bfe
Jack-Tillman <johntillman345@gmail.com>
2023-10-20 11:40:22 -0400

    Created games db file with createGame function, added table to createTables; error thrown when seeding database, so not finished yet

---
+++ src/server/db/games.js
@@ -0,0 +1,19 @@
+const db = require('./client');
+
+const createGame = async({productName, price, productImage, genre, playerRange, esrb, condition, publisher, description, delivery}) => {
+    try {
+        const { rows: [game ] } = await db.query(`
+        INSERT INTO games(productName, price, productImage, genre, playerRange, esrb, condition, publisher, description, delivery)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, 10$)
+        ON CONFLICT (productName) DO NOTHING
+        RETURNING *`, [productName, price, productImage, genre, playerRange, esrb, condition, publisher, description, delivery]);
+
+        return game;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports ={
+    createGame
+}
--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,85 +1,157 @@
-const db = require('./client');
-const { createUser } = require('./users');
+/*
+Oct. 20 Note: 
+This is still not functioning!
+Currently, seed the database throws an error while inserting seed data "syntax error at or near $" starting at createGame
+Things i've tried: changing price data type, making everything lowercase 
+*/
+
+
+
+
+const db = require("./client");
+const { createUser } = require("./users");
+const { createGame } = require("./games");
 
 const users = [
   {
-    name: 'Emily Johnson',
-    email: 'emily@example.com',
-    password: 'securepass',
+    name: "Emily Johnson",
+    email: "emily@example.com",
+    password: "securepass",
+    isAdmin: true,
   },
   {
-    name: 'Liu Wei',
-    email: 'liu@example.com',
-    password: 'strongpass',
+    name: "Liu Wei",
+    email: "liu@example.com",
+    password: "strongpass",
+    isAdmin: false,
   },
   {
-    name: 'Isabella García',
-    email: 'bella@example.com',
-    password: 'pass1234',
+    name: "Isabella García",
+    email: "bella@example.com",
+    password: "pass1234",
+    isAdmin: false,
   },
   {
-    name: 'Mohammed Ahmed',
-    email: 'mohammed@example.com',
-    password: 'mysecretpassword',
+    name: "Mohammed Ahmed",
+    email: "mohammed@example.com",
+    password: "mysecretpassword",
+    isAdmin: false,
   },
   {
-    name: 'John Smith',
-    email: 'john@example.com',
-    password: 'password123',
+    name: "John Smith",
+    email: "john@example.com",
+    password: "password123",
+    isAdmin: false,
   },
   // Add more user objects as needed
-];  
+];
+
+const games = [
+  {
+    productName: "Video GAME",
+    price: 39.99,
+    productImage: "imgur.com",
+    genre: "Action",
+    playerRange: "Singleplayer",
+    esrb: "T",
+    condition: "New",
+    publisher: "VideoGame Pub",
+    description: "This is the video game of all time",
+    delivery: "Same Day",
+  },
+];
 
 const dropTables = async () => {
-    try {
-        await db.query(`
+  try {
+    await db.query(`
         DROP TABLE IF EXISTS users;
-        `)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+        DROP TABLE IF EXISTS games;
+        `);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const createTables = async () => {
-    try{
-        await db.query(`
+  try {
+    await db.query(`
         CREATE TABLE users(
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) DEFAULT 'name',
             email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
-        )`)
-    }
-    catch(err) {
-        throw err;
-    }
-}
+            password VARCHAR(255) NOT NULL,
+            isAdmin BOOLEAN default false
+        );
+        
+        CREATE TABLE games(
+          id SERIAL PRIMARY KEY,
+          productName VARCHAR(255) NOT NULL,
+          price FLOAT NOT NULL,
+          productImage VARCHAR(255) NOT NULL,
+          genre VARCHAR(255) NOT NULL, 
+          playerRange VARCHAR(255) NOT NULL,
+          esrb VARCHAR(255) NOT NULL,
+          condition VARCHAR(255) NOT NULL,
+          publisher VARCHAR(255) NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          delivery VARCHAR(255) NOT NULL
+      );
+        `);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const insertUsers = async () => {
   try {
     for (const user of users) {
-      await createUser({name: user.name, email: user.email, password: user.password});
+      await createUser({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin,
+      });
     }
-    console.log('Seed data inserted successfully.');
+    console.log("Seed data inserted successfully.");
   } catch (error) {
-    console.error('Error inserting seed data:', error);
+    console.error("Error inserting seed data:", error);
+  }
+};
+
+const insertGames = async () => {
+  try {
+    for (const game of games) {
+      await createGame({
+        productName: game.productName,
+        price: game.price,
+        productImage: game.productImage,
+        genre: game.genre,
+        playerRange: game.playerRange,
+        esrb: game.esrb,
+        condition: game.condition,
+        publisher: game.publisher,
+        description: game.description,
+        delivery: game.delivery,
+      });
+    }
+    console.log("Seed data inserted successfully.");
+  } catch (error) {
+    console.error("Error inserting seed data:", error);
   }
 };
 
 const seedDatabse = async () => {
-    try {
-        db.connect();
-        await dropTables();
-        await createTables();
-        await insertUsers();
-    }
-    catch (err) {
-        throw err;
-    }
-    finally {
-        db.end()
-    }
-}
+  try {
+    db.connect();
+    await dropTables();
+    await createTables();
+    await insertUsers();
+    await insertGames();
+  } catch (err) {
+    throw err;
+  } finally {
+    db.end();
+  }
+};
 
-seedDatabse()
+seedDatabse();
