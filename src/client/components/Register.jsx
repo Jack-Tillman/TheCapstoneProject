1300b756a281655f682e66f3c1e1ee78717cca3a
duncanharrelson <duncan.harrelson@gmail.com>
2023-10-25 10:42:31 -0500

    add CartContext and password validation to Register

--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { registerUser } from "../api/index";
+import PasswordChecklist from "react-password-checklist";
 
 
 export const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordAgain, setPasswordAgain] = useState("");
     const [error, setError] = useState(null);
 
     async function handleSubmit(event) {
@@ -33,7 +35,24 @@ export const Register = () => {
                     Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} minLength={8} required/>
                 </label>
                 <br />
-                <button type="submit">Submit</button>
+                <label>
+                    Confirm Password: <input type="password" value={password} onChange={(e) => setPasswordAgain(e.target.value)} minLength={8} required/>
+                </label>
+                <PasswordChecklist
+                    rules={["minLength", "specialChar", "number", "capital", "match"]}
+                    minLength={8}
+                    value={password}
+                    valueAgain={passwordAgain}
+                    messages={{
+                        minLength: "Password must be 8 characters or more.",
+                        specialChar: "Password must contain a special character",
+                        number: "Password must contain a number",
+                        capital: "Password must contain a capital letter",
+                        match: "Password fields must match"
+                    }}
+                />
+
+                <button id="submit" type="submit">Submit</button>
             </form>        
         </>
     ) 
