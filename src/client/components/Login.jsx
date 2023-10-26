ec8c5834013733c49b9c2c59e2ad37a57daf09c7
“alexlatch” <annalatiolais1@icloud.com>
2023-10-26 11:20:01 -0500

    login and register inputs look prettier, made with MUI elements

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
 import { userLogin } from '../api/index';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
@@ -13,6 +25,14 @@ export const Login = () => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  };
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
   const handleSubmit = async(e) => {
@@ -29,28 +49,62 @@ export const Login = () => {
   };
 
   return (
+    
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='email'>Email:</label>
-          <input
+          {/*<label htmlFor='email'>Email:</label>
+           <input
             type='email'
             id='email'
             value={email}
             onChange={handleEmailChange}
             required
-          />
+          /> */}
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            value={email}
+            onChange={handleEmailChange}
+          /></FormControl>
         </div>
         <div>
-          <label htmlFor='password'>Password:</label>
+          {/* <label htmlFor='password'>Password:</label>
           <input
             type='password'
             id='password'
             value={password}
             onChange={handlePasswordChange}
             required
+          /> */}
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            // type='password'
+            // id='password'
+            value={password}
+            onChange={handlePasswordChange}
+            required
           />
+          </FormControl>
         </div>
         <button type='submit'>Login</button>
       </form>
