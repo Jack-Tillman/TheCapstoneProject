00f1e03a8903f1b62df0e1826bf9bbd8dbd8b174
“alexlatch” <annalatiolais1@icloud.com>
2023-11-04 20:21:17 -0500

    changed button on logout page to mui button

--- src/client/components/Logout.jsx
+++ src/client/components/Logout.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const Logout = () => {
   sessionStorage.removeItem("token");
@@ -8,7 +10,7 @@ export const Logout = () => {
       <div id="success-notification">
         <h1>Logged out!</h1>
         <p>Click below to return home</p>
-        <button
+        {/* <button
           id="home-btn"
           onClick={(e) => {
             e.preventDefault();
@@ -16,7 +18,8 @@ export const Logout = () => {
           }}
         >
           Home
-        </button>
+        </button> */}
+        <Link to="/"><Button variant="contained">Home</Button></Link>
       </div>
     </>
   )
