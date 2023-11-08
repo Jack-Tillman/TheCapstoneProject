002790ef2fce03170664b9c62e82db75194f4076
“alexlatch” <annalatiolais1@icloud.com>
2023-11-08 11:17:18 -0600

    unfinished theme

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,51 +1,60 @@
 import { useState, useEffect } from 'react'
-import { ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 import MainSection from './components/MainSection';
 import NavBar from './components/NavBar';
 import createTheme from '@mui/material/styles/createTheme';
 import { useTheme } from '@mui/material/styles';
 import { red, purple } from '@mui/material/colors';
 import { CartProvider } from './CartContext';
+import { PaletteMode } from '@mui/material';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500],
-    },
-    secondary: {
-      main: purple[500],
-    },
-  },
-});
-
-// const getDesignTokens = (mode: PaletteMode) => ({
+// const theme = createTheme({
 //   palette: {
-//     mode,
-//     ...(mode === 'light'
-//       ? {
-//           // palette values for light mode
-//           primary: amber,
-//           divider: amber[200],
-//           text: {
-//             primary: grey[900],
-//             secondary: grey[800],
-//           },
-//         }
-//       : {
-//           // palette values for dark mode
-//           primary: deepOrange,
-//           divider: deepOrange[700],
-//           background: {
-//             default: deepOrange[900],
-//             paper: deepOrange[900],
-//           },
-//           text: {
-//             primary: '#fff',
-//             secondary: grey[500],
-//           },
-//         }),
+//     background: {
+//       light: "#fff",
+//       dark: "#414141",
+//     },
+//     text: {
+//       light: "#000000",
+//       dark: "#fff",
+//     },
+//     primary: {
+//       main: red[500],
+//     },
+//     secondary: {
+//       main: purple[500],
+//     },
 //   },
 // });
+
+const getDesignTokens = () => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+          // palette values for light mode
+          primary: amber,
+          divider: amber[200],
+          text: {
+            primary: grey[900],
+            secondary: grey[800],
+          },
+        }
+      : {
+          // palette values for dark mode
+          primary: deepOrange,
+          divider: deepOrange[700],
+          background: {
+            default: deepOrange[900],
+            paper: deepOrange[900],
+          },
+          text: {
+            primary: '#fff',
+            secondary: grey[500],
+          },
+        }),
+  },
+});
 
 
 function App() {
@@ -64,21 +73,21 @@ function App() {
     getToken(storageToken);
   },[token, storageToken]);
 
-    // const [mode, setMode] = React.useState<PaletteMode>('light');
-  // const colorMode = React.useMemo(
-  //   () => ({
-  //     // The dark mode switch would invoke this method
-  //     toggleColorMode: () => {
-  //       setMode((prevMode: PaletteMode) =>
-  //         prevMode === 'light' ? 'dark' : 'light',
-  //       );
-  //     },
-  //   }),
-  //   [],
-  // );
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode(() =>
+          prevMode === 'light' ? 'dark' : 'light',
+        );
+      },
+    }),
+    [],
+  );
 
-  // // Update the theme only if the mode changes
-  // const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  // Update the theme only if the mode changes
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   
   // return (
   //   <div className='App'>
@@ -91,17 +100,16 @@ function App() {
 
   
   return (  
-    // <ColorModeContext.Provider value={colorMode}>
+    <ColorModeContext.Provider value={colorMode}>
     <CartProvider>
       <ThemeProvider theme={theme}>    
         <div className='App'>
           <NavBar token={token} setToken={setToken} />
-
           <MainSection token={token} setToken={setToken} />
         </div>
       </ThemeProvider>
     </CartProvider>
-    // </ColorModeContext.Provider>
+    </ColorModeContext.Provider>
   );
 }
 
