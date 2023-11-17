055d1bd76d49432eaa2b53c0e0c18fa1521a5077
Jack-Tillman <johntillman345@gmail.com>
2023-11-17 17:04:09 -0500

    this sucks

--- src/client/components/ProductCard.jsx
+++ src/client/components/ProductCard.jsx
@@ -1,11 +1,10 @@
-import { Card, Form, Row, Col, Image } from 'react-bootstrap';
-import { Box } from '@mui/material';
+import { Card, Form, Row, Col, Image } from "react-bootstrap";
+import { Box } from "@mui/material";
 // import { Modal } from '@mui/material';
-import { Button, ButtonGroup } from '@mui/material';
+import { Button, ButtonGroup } from "@mui/material";
 import { CartContext } from "../CartContext";
 import { useContext, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 export function ProductCard(props) {
   const product = props.product;
@@ -16,27 +15,25 @@ export function ProductCard(props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  return (  
+  return (
     <>
-      
-
-    <Box 
-      width={250}
-      gap={0}
-      sx={{
-        margin: 2,
-        padding: 1,
-        borderColor: "primary",
-        borderWidth: 1,        
-        borderStyle: "solid",
-        borderRadius: 3,
-        boxShadow: "0px",
-        '&:hover': {
-          boxShadow: "0px 0px 10px black",
-        },
-      }}
-    >
-      {/* <Modal show={show} onHide={handleClose}>
+      <Box
+        width={250}
+        gap={0}
+        sx={{
+          margin: 2,
+          padding: 1,
+          borderColor: "primary",
+          borderWidth: 1,
+          borderStyle: "solid",
+          borderRadius: 3,
+          boxShadow: "0px",
+          "&:hover": {
+            boxShadow: "0px 0px 10px black",
+          },
+        }}
+      >
+        {/* <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{product.productname}</Modal.Title>
                 </Modal.Header>
@@ -48,32 +45,61 @@ export function ProductCard(props) {
                     <p><strong>Description:</strong> {product.description}</p>                        
                 </Modal.Body>
             </Modal> */}
-    <Card>      
-      <Card.Body>          
-          <Card.Img src={`${product.productimage}`} className="smallImage"/>
-          <Card.Title>{product.productname}</Card.Title>
-          <Card.Text>${product.price}</Card.Text>
-          {/* <Button onClick={handleShow}>More Info</Button> */}
-          { productQuantity > 0 ?
-          <>
-            <Form as={Row}>
-            <Form.Label column="true" sm="6"><strong>In Cart: {productQuantity}</strong></Form.Label><br />
-              <ButtonGroup size="small" variant="outlined" aria-label="add/remove item from cart">
-                <Button onClick={() => cart.removeOneFromCart(product.stripe_id)}>-</Button>
-                <Button onClick={() => cart.addOneToCart(product.stripe_id)}>+</Button>
-              </ButtonGroup>  
-            </Form>
-            <Button sx={{marginTop: 1,}} variant="outlined" onClick={() => cart.deleteFromCart(product.stripe_id)}>Remove all from Cart</Button>
-          </>    
-          :
-          <Button variant="contained" onClick={() => cart.addOneToCart(product.stripe_id, product.price, product.productname)}>Add To Cart</Button>
-        }
-      </Card.Body>
-    </Card>    
-    </Box>
-
-    
-
-  </> 
+        <Card>
+          <Card.Body>
+            <Card.Img src={`${product.productimage}`} className="smallImage" />
+            <Card.Title>{product.productname}</Card.Title>
+            <Card.Text>${product.price}</Card.Text>
+            {/* <Button onClick={handleShow}>More Info</Button> */}
+            {productQuantity > 0 ? (
+              <>
+                <Form as={Row}>
+                  <Form.Label column="true" sm="6">
+                    <strong>In Cart: {productQuantity}</strong>
+                  </Form.Label>
+                  <br />
+                  <ButtonGroup
+                    size="small"
+                    variant="outlined"
+                    aria-label="add/remove item from cart"
+                  >
+                    <Button
+                      onClick={() => cart.removeOneFromCart(product.stripe_id)}
+                    >
+                      -
+                    </Button>
+                    <Button
+                      onClick={() => cart.addOneToCart(product.stripe_id)}
+                    >
+                      +
+                    </Button>
+                  </ButtonGroup>
+                </Form>
+                <Button
+                  sx={{ marginTop: 1 }}
+                  variant="outlined"
+                  onClick={() => cart.deleteFromCart(product.stripe_id)}
+                >
+                  Remove all from Cart
+                </Button>
+              </>
+            ) : (
+              <Button
+                variant="contained"
+                onClick={() =>
+                  cart.addOneToCart(
+                    product.stripe_id,
+                    product.price,
+                    product.productname
+                  )
+                }
+              >
+                Add To Cart
+              </Button>
+            )}
+          </Card.Body>
+        </Card>
+      </Box>
+    </>
   );
-};
+}
