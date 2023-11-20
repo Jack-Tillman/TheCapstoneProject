5b8ce8b0f3d1d190d2a15f3fb7639ce444bdc9a9
“alexlatch” <annalatiolais1@icloud.com>
2023-11-19 19:48:18 -0600

    added text to carousel

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -22,19 +22,23 @@ return (
 <div className='container'>
     <div className = "carousel">
    <Slider {...settings}>
-       <div>
-           <img className = "carouselimg" src="https://i.ytimg.com/vi/7ZXTMNHpjXA/maxresdefault.jpg" alt="Call of Duty Gameplay" />
+       <div className="slideDiv">
+            <h1 className='carouselText'>Call of Duty</h1>
+            <img className = "carouselimg" src="https://i.ytimg.com/vi/7ZXTMNHpjXA/maxresdefault.jpg" alt="Call of Duty Gameplay" />
         </div>
 
-         <div>
+        <div className="slideDiv">
+        <h1 className='carouselText'>FIFA 23</h1>
            <img className = "carouselimg" src="https://assetsio.reedpopcdn.com/fifa-23-again.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp" alt="FIFA 23 Gameplay" />
        </div>
 
-        <div>
+        <div className="slideDiv">
+        <h1 className='carouselText'>Skyrim</h1>
          <img className = "carouselimg" src="https://i.ytimg.com/vi/kQ6tCc5dFYc/maxresdefault.jpg" alt="Skyrim, The Elder Scrolls Gameplay" />
         </div>
 
-        <div>
+        <div className="slideDiv">
+        <h1 className='carouselText'>The Legend of Zelda</h1>
          <img className = "carouselimg" src="https://cdn.vox-cdn.com/thumbor/0PrsfU_7JARyXrcxuRHM3wdHp3E=/0x0:1024x576/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/53525779/zeldabreath.0.jpg" />
         </div>
         
